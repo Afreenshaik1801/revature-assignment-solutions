186e8862d96751e73711f86cdb7f8e432650742e
yeshwanth6134 <yeshwanth6134@gmail.com>
2025-10-15 21:45:50 +0530

    Delete yeshwanth/A1.14-10-2025 directory

--- yeshwanth/A1.14-10-2025/even.py
+++
@@ -1,6 +0,0 @@
-for i in range(1, 11):
-    if i % 2 == 0:
-        print("Even number found:", i)
-        break
-else:
-    print("No even numbers found.")
--- yeshwanth/A1.14-10-2025/product.py
+++
@@ -1,5 +0,0 @@
-a=[10,20,30,40,50]
-n=1
-for i in a:
-    n *= i
-print("product of elements",n)  
